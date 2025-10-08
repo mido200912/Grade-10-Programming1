e1252d0ed47a222824e286c659cdd5ea15687c5e
mido200912 <mohammedalsyedalsyed12@gmail.com>
2025-10-08 15:22:35 +0300

    Update Icons.tsx

--- components/icons/Icons.tsx
+++ components/icons/Icons.tsx
@@ -159,3 +159,11 @@ export const User: React.FC<IconProps> = (props) => (
         <circle cx="12" cy="7" r="4" />
     </svg>
 );
+
+export const Menu: React.FC<IconProps> = (props) => (
+  <svg {...props} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+    <line x1="3" y1="12" x2="21" y2="12" />
+    <line x1="3" y1="6" x2="21" y2="6" />
+    <line x1="3" y1="18" x2="21" y2="18" />
+  </svg>
+);
